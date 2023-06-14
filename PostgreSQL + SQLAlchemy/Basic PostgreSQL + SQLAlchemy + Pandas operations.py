3e6383d34d6181bdefccd94606fe186c1332dd38
Dawid <dawidniewdana@gmail.com>
2023-06-14 08:37:51 +0200

    Learning PostgreSQL and SQLAlchemy
    Addiing update function to the existing scripts + minor corrections

--- PostgreSQL + SQLAlchemy/Basic PostgreSQL + SQLAlchemy + Pandas operations.py
+++ PostgreSQL + SQLAlchemy/Basic PostgreSQL + SQLAlchemy + Pandas operations.py
@@ -11,7 +11,7 @@ import pandas as pd
 
 ###connect to PostgreSQL table
 engine = sqlalchemy.create_engine('postgresql+psycopg2://USER_NAME:PASSWORD@localhost/DATABASE_NAME')
-table_prepared = pd.read_csv(r"FILE_PATH")
+table_from_other_source = pd.read_csv(r"FILE_PATH")
 print (f'A connection to PostgreSQL was successfully established')
 
 ###creates a table in PostgreSQL database
@@ -32,9 +32,9 @@ print (f'A table {table_name} was succesfully created')
 ###adding values into database
 counter = 0
 
-for row in range (len(table_prepared)):
-    salary = table_prepared.iloc[counter,1]
-    years_of_experience = table_prepared.iloc[counter,0]
+for row in range (len(table_from_other_source)):
+    salary = table_from_other_source.iloc[counter,1]
+    years_of_experience = table_from_other_source.iloc[counter,0]
     index_to_be_passed = counter
     with engine.connect() as conn:
         conn.execute(sqlalchemy.insert(table).
@@ -49,7 +49,27 @@ for row in range (len(table_prepared)):
         pass
     counter +=1
 
-print (f'Values were added to the table. Values added: {len(table_prepared}')
+print (f'Values were added to the table. Values added: {len(table_from_other_source)}')
+
+
+###updating a value in a database
+
+for row in range (len(table_from_other_source)):
+    index_to_be_updated = counter
+    new_salary = table_from_other_source.iloc[counter,0] ###changes later on to the file with updated data
+    with engine.connect() as conn:
+        conn.execute(sqlalchemy
+                     .update(table)
+                     .where(table.c.number==index_to_be_updated)
+                     .values
+                     (Salary=new_salary)
+                    )
+        conn.commit()
+    if counter % 10 == 0:
+        print (f'Updated {counter} rows')
+    else:
+        pass
+    counter +=1
 
 ###load PostgreSQL table into Pandas DF
 
